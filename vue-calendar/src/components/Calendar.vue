786b7cb06abdb864fc7594f079d4630c1c4686f8
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2019-12-18 01:05:43 +0100

    Update Calendar.vue

--- vue-calendar/src/components/Calendar.vue
+++ vue-calendar/src/components/Calendar.vue
@@ -230,7 +230,7 @@ export default {
     
     async logOut () {
       await firebase.auth().signOut();
-      window.location.href ='http://www.george-adrian.dk/pracownia_eksperymentu';
+      window.location.href ='http://www.george-adrian.dk/pracownia_eksperymentu/index.html';
     },
  
     setDialogDate( { date }) {
